869d9ccac9375e9746451fe269bd9c61f419f6a6
dd-embuild <78391959+dd-embuild@users.noreply.github.com>
2021-02-01 21:21:02 -0500

    Update main.cpp

--- .github/workflows/main.cpp
+++ .github/workflows/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
+class Entity {
+  private:
+    int health;
+    const char *name;
+    void AdjustHealth(int delta);
+    void ShowHealth(void);
+  public:
+    Entity(const char *name);
+    void AddHealth(int delta);
+    void AddDamage(int delta);
+};
+
+Entity::Entity(const char *name="anon")
+{
+    this->name = name;
+    this->health = 10;
+}
+
+void Entity::ShowHealth(void)
+{
+    std::cout << this->name << "'s health is " << this->health << "\n";   
+}
+
+void Entity::AdjustHealth(int delta)
+{
+    this->health += delta;
+    this->ShowHealth();
+}
+
+void Entity::AddDamage(int delta)
+{
+    this->AdjustHealth(-1 * delta);
+}
+
+void Entity::AddHealth(int delta)
+{
+    this->AdjustHealth(delta);   
+}
+
 int main(void)
 {
+    Entity joe("Joe);
     std::cout << "Hello, world!\n";
 }
